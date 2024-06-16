220e14ee3045d0645e3e8defc45e8606c86f9bef
Guw2 <gustafar79@gmail.com>
2024-06-15 23:00:23 -0300

    First Servlet App

---
+++ src/main/java/com/lorian/FirstServlet.java
@@ -0,0 +1,34 @@
+package com.lorian;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class FirstServlet extends HttpServlet{
+
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		
+		int num1 = Integer.parseInt(req.getParameter("num1"));
+		int num2 = Integer.parseInt(req.getParameter("num2"));
+		
+		int result = num1 + num2;
+		
+		req.setAttribute("result", result);
+		
+		if(num1 > num2) {			
+			RequestDispatcher rd = req.getRequestDispatcher("/servlet_add2");
+			rd.forward(req, res);
+		}
+		
+		PrintWriter out = res.getWriter();
+		
+		out.println("The result is " + result + " in FirstServlet.");
+		
+	}
+	
+}
