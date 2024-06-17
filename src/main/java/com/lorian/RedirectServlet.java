b02c3d75aab42f94e8084d824803680b4ea3a064
Guw2 <gustafar79@gmail.com>
2024-06-17 17:05:47 -0300

    Major Update : Cookies and HttpSession added, using SendRedirect in a Redirect servlet, adding better logic and better code organization.

---
+++ src/main/java/com/lorian/RedirectServlet.java
@@ -0,0 +1,31 @@
+package com.lorian;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class RedirectServlet extends HttpServlet{
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		
+		int num1 = Integer.parseInt(req.getParameter("num1"));
+		int num2 = Integer.parseInt(req.getParameter("num2"));
+		
+		HttpSession session = req.getSession();
+		session.setAttribute("num1", num1);
+		session.setAttribute("num2", num2);
+		
+		Cookie cookie = new Cookie("message", "hifromredirectservlet");
+		res.addCookie(cookie);
+		
+		res.sendRedirect("servlet_add");
+		
+	}
+	
+}
